1cfeaa7093516fcb0619ed4bd395a9fdbbc1554c
VIFilip <vlad_1611@yahoo.com>
2018-03-06 19:26:15 +0200

    Created maven project

---
+++ ElectricVehiclePurchase/src/main/java/ro/siit/java10/evp/GreenBonus.java
@@ -0,0 +1,14 @@
+package ro.siit.java10.electrivehicle;
+
+public class GreenBonus {
+
+    float fixedBudget;
+
+//Class Hystory is contained by Green Bonus Class
+
+class Hystory {
+    String clientname;
+    String dealershipname;
+    String carmodel;
+}
+}
---
+++ ElectricVehiclePurchase/src/main/java/ro/siit/java10/evp/Hystory.java
@@ -0,0 +1,9 @@
+package ro.siit.java10.electrivehicle;
+
+public class Hystory {
+
+    String clientname;
+    String dealershipname;
+    String carmodel;
+
+}
